b7eefb293e9ae883dca9a78cbf4a0a9645c989b8
Samwel Salim <132700524+Sparkssam@users.noreply.github.com>
2026-06-21 00:48:20 +0300

    Delete app directory

--- app/admin/database/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DatabaseViewer } from "@/components/admin/database-viewer"
-
-export default function AdminDatabasePage() {
-  return (
-    <div className="container mx-auto py-6">
-      <DatabaseViewer />
-    </div>
-  )
-}
