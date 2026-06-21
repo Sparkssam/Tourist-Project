1f297f760c8586d34ddcb595145770908366df9a
Samwel Salim <132700524+Sparkssam@users.noreply.github.com>
2026-06-21 11:03:10 +0300

    Delete app directory

--- app/admin/progress/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ProgressMonitor } from "@/components/admin/progress-monitor"
-
-export default function AdminProgressPage() {
-  return (
-    <div className="container mx-auto py-6">
-      <ProgressMonitor />
-    </div>
-  )
-}
--- app/dashboard/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function DashboardRedirectPage() {
-  // This page will be handled by middleware for role-based redirection
-  redirect('/tourist')
-}
