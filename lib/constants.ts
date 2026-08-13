33f50eea963ed2e669de15a318f6595e7f263364
Sparkssam <samsuya999@gmail.com>
2026-08-14 02:00:29 +0300

    Set direct Google Business review link for Kekeo Safaris

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const GOOGLE_REVIEW_URL =
   process.env.NEXT_PUBLIC_GOOGLE_REVIEW_URL ||
-  "https://www.google.com/maps/search/?api=1&query=Kekeo+Safaris+Arusha+Tanzania"
+  "https://g.page/r/CWYaAzWVtitlEBM/review"
 
 export const TRIPADVISOR_URL =
   process.env.NEXT_PUBLIC_TRIPADVISOR_URL || "https://www.tripadvisor.com"
